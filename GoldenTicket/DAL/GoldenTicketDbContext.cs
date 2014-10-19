bbf93004cdecfd9f82bd6190901fea3a543bfd22
Jeff Maher <jeff@codeforamerica.org>
2014-10-19 18:11:59 -0400

    Added initial model and some sample seed data.

---
+++ GoldenTicket/DAL/GoldenTicketDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+using GoldenTicket.Models;
+
+namespace GoldenTicket.DAL
+{
+    public class GoldenTicketDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Program> Programs { get; set; }
+        public DbSet<Applied> Applieds { get; set; }
+        public DbSet<Shuffled> Shuffleds { get; set; }
+        public DbSet<Selected> Selecteds { get; set; }
+        public DbSet<Waitlisted> Waitlisteds { get; set; }
+    }
+}
