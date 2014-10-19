9f7e9adcdc1cec02e80a54e803e5248de85b7e7c
Jeff Maher <jeff@codeforamerica.org>
2014-10-19 19:43:03 -0400

    Installed JS and CSS assets, as well as prototype Parent screens.

---
+++ GoldenTicket/Controllers/RegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoldenTicket.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StudentInformation()
+        {
+            return View();
+        }
+
+        public ActionResult GuardianInformation()
+        {
+            return View();
+        }
+
+        public ActionResult SchoolSelection()
+        {
+            return View();
+        }
+        public ActionResult Review()
+        {
+            return View();
+        }
+
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
---
+++ GoldenTicket/Scripts/select.jquery.js
@@ -0,0 +1,58 @@
+/*
+ * select plugin
+ *
+ * Copyright (c) 2013 Filament Group, Inc.
+ * Licensed under MIT
+ */
+
+(function( w, $ ) {
+
+	var pluginName = "select",
+		initSelector = "select",
+		omitSelector = "",
+		cl = {
+			select: "custom-" + pluginName,
+			text: "label-text",
+			btn: "btn-" + pluginName
+		};
+
+	$.fn[ pluginName ] = function(){
+		
+		return this.each(function(){
+			var $s = $( this ),
+				$p = $s.parent().addClass( cl.select );
+
+			/* check if opacity value matches what's in the CSS to limit the enhancement to browsers that support opacity in the same way (otherwise the select may only partially enhance and be unusable) */
+			if ( $s.css( "opacity" ) >= 0.001 ) { 
+				var $l = $s.prev().addClass( cl.text ),				
+					getSelectText = function(){
+						return $s[ 0 ].options[ $s[ 0 ].selectedIndex ].text;
+					},
+					$btn = $( "<span class='"+ cl.btn +"'>" + getSelectText() + "</span>" );
+
+				$s
+					.before( $btn )
+					.bind( "change", function(){
+						$btn.html( getSelectText() );
+					})
+					.bind( "focus", function(){
+						$btn.addClass( "btn-focus" );
+					})
+					.bind( "blur", function(){
+						$btn.removeClass( "btn-focus" );
+					});
+
+			}
+			else {
+				$s.css( "opacity", "1" );
+			};			
+		});
+	};
+
+	// auto-init
+	$(function(){
+		$( initSelector )[ pluginName ]();
+	});
+
+}( this, jQuery ));
+
