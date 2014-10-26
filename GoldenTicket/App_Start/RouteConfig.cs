6382553d8a38e3458cceba5b961c5541c06c0345
Jeff Maher <jeff@codeforamerica.org>
2014-10-26 15:48:29 -0400

    Set default route to Registration Index

--- GoldenTicket/App_Start/RouteConfig.cs
+++ GoldenTicket/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GoldenTicket
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Registration", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
