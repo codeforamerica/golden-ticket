cd1bc0bcb64156f1698d5dce7ffa5d6562fc8486
Jeff Maher <jeff@codeforamerica.org>
2014-11-06 18:16:22 -0800

    Exposed a temporary testing page at `/Tester`

---
+++ GoldenTicket/Controllers/TesterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoldenTicket.Models;
+using GoldenTicket.DAL;
+
+namespace GoldenTicket.Controllers
+{
+    public class TesterController : Controller
+    {
+        private readonly GoldenTicketDbContext database = new GoldenTicketDbContext();
+
+        // GET: Tester
+        public ActionResult Index()
+        {
+            ViewBag.Applicants = database.Applicants.ToList();
+
+            return View();
+        }
+    }
+}
