8f3ba51cbbfe1ab22c74de1beeaf6990bd928c48
Jeff Maher <jeff@codeforamerica.org>
2014-11-14 15:32:12 -0800

    Changed namespace of translation file.

--- GoldenTicket/Models/Applicant.cs
+++ GoldenTicket/Models/Applicant.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using GoldenTicketResources;
+using GoldenTicket.Resources;
 
 
 namespace GoldenTicket.Models
--- GoldenTicket/Resources/GoldenTicketText.designer.cs
+++ GoldenTicket/Resources/GoldenTicketText.designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoldenTicketResources {
+namespace GoldenTicket.Resources {
     using System;
     
     
