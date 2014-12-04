d136ce6747be7b2b10f37c079c870d5526b7cd5e
Jeff Maher <jeff.m.maher@gmail.com>
2014-12-03 22:33:16 -0800

    Added a helper to send e-mails, along with a default configuration.

---
+++ GoldenTicket/Misc/EmailHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace GoldenTicket.Misc
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string toAddress, string fromAddress, string subject, string messageBody)
+        {
+            // Setup the server
+            var mailServerAddress = ConfigurationManager.AppSettings["SmtpAddress"];
+            var mailServerPort = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+            var mailServerUsername = ConfigurationManager.AppSettings["SmtpUsername"];
+            var mailServerPassword = ConfigurationManager.AppSettings["SmtpPassword"];
+            
+            var mailClient = new SmtpClient(mailServerAddress, mailServerPort);
+            mailClient.Credentials = new NetworkCredential(mailServerUsername, mailServerPassword);
+
+            var mailMessage = new MailMessage(to: toAddress, @from: fromAddress, subject: subject, body: messageBody);
+            mailMessage.IsBodyHtml = true;
+            mailClient.Send(mailMessage);
+        }
+
+        public static void SendEmail(string toAddress, string subject, string messageBody)
+        {
+            SendEmail(toAddress, ConfigurationManager.AppSettings["MailFrom"], subject, messageBody);
+        }
+
+    }
+}
