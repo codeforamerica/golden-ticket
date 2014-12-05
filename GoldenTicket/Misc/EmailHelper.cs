b9811d076fb9606cbbf7db876f0737a757d005af
Jeff Maher <jeff.m.maher@gmail.com>
2014-12-05 12:23:13 -0800

    Commented the EmailHelper functionality

--- GoldenTicket/Misc/EmailHelper.cs
+++ GoldenTicket/Misc/EmailHelper.cs
@@ -8,15 +8,17 @@ namespace GoldenTicket.Misc
     {
         public static void SendEmail(string toAddress, string fromAddress, string subject, string messageBody)
         {
-            // Setup the server
+            // Get the configuration settings
             var mailServerAddress = ConfigurationManager.AppSettings["SmtpAddress"];
             var mailServerPort = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
             var mailServerUsername = ConfigurationManager.AppSettings["SmtpUsername"];
             var mailServerPassword = ConfigurationManager.AppSettings["SmtpPassword"];
             
+            // Setup the SMTP connection 
             var mailClient = new SmtpClient(mailServerAddress, mailServerPort);
             mailClient.Credentials = new NetworkCredential(mailServerUsername, mailServerPassword);
 
+            // Send the message
             var mailMessage = new MailMessage(to: toAddress, @from: fromAddress, subject: subject, body: messageBody);
             mailMessage.IsBodyHtml = true;
             mailClient.Send(mailMessage);
