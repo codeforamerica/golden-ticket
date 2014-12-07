741951eaffe2a18a8f42cd0ab35ca6764f3851d2
Jeff Maher <jeff.m.maher@gmail.com>
2014-12-07 14:49:17 -0800

    Added documentation for RegistrationController.

--- GoldenTicket/Controllers/RegistrationController.cs
+++ GoldenTicket/Controllers/RegistrationController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Data.Entity;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using GoldenTicket.Misc;
 using GoldenTicket.Models;
@@ -13,22 +10,37 @@ using GoldenTicket.Resources;
 
 namespace GoldenTicket.Controllers
 {
+    /**
+     * <summary>
+     * This controller handles all views and submissions related to the parent or applicant
+     * side of Golden Ticket. They methods contain all the functionality in the registration
+     * process.
+     * </summary>
+     **/
     public class RegistrationController : Controller
     {
+        // Database connection for interacting with model objects
         private readonly GoldenTicketDbContext database = new GoldenTicketDbContext();
-        private GlobalConfig config;
 
-        
+        // Contains helper functions shared by other controllers
+        private readonly SharedViewHelper viewHelper;
 
-        private SharedViewHelper viewHelper;
-
+        /**
+         * <summary>
+         * Configures any instance variables that can't be
+         * initialized earlier.
+         * </summary>
+         **/ 
         public RegistrationController()
         {
-            config = database.GlobalConfigs.First();
             viewHelper = new SharedViewHelper(database);
         }
 
-        // GET: Registration
+        /**
+         * <summary>
+         * Clears the session and presents a welcome message to applicants
+         * </summary>
+         * */
         public ActionResult Index()
         {
             Session.Clear();
@@ -38,15 +50,29 @@ namespace GoldenTicket.Controllers
             return View();
         }
 
+        /**
+         * <summary>
+         * Shows a form for student information.
+         * </summary>
+         * */
         public ActionResult StudentInformation()
         {
+            // Add display variables to the ViewBag
             viewHelper.PrepareStudentInformationView(ViewBag);
 
+            // Create an applicant in the database
             var applicant = GetSessionApplicant();
 
             return View(applicant);
         }
 
+        /**
+         * <summary>
+         * Saves data entered in the student information form.
+         * </summary>
+         * 
+         * <param name="applicant">Applicant object containing student information fields</param>
+         * */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult StudentInformation(Applicant applicant)
@@ -60,7 +86,7 @@ namespace GoldenTicket.Controllers
             // Check for required fields
             viewHelper.EmptyCheckStudentInformation(ModelState, applicant);
 
-            // Valid fields
+            // Validate fields
             if(ModelState.IsValid)
             {
                 SaveStudentInformation(applicant);
@@ -73,20 +99,34 @@ namespace GoldenTicket.Controllers
             return View(applicant);
         }
 
+        /**
+         * <summary>
+         * Display the form to collect guardian and alternate contact person information.
+         * </summary>
+         * */
         public ActionResult GuardianInformation()
         {
+            // Validate the session
             if (!IsActiveSession()) //TODO Do this with AOP/Annotations instead
             {
                 return RedirectToAction("Index");
             }
 
+            // Get data for view variables
             viewHelper.PrepareGuardianInformationView(ViewBag);
-
             var applicant = GetSessionApplicant();
+
 
             return View(applicant); 
         }
 
+        /**
+         * <summary>
+         * Save data collected from the guardian information form
+         * </summary>
+         * 
+         * <param name="applicant">Applicant object with guardian and contact person fields</param>
+         * */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult GuardianInformation(Applicant applicant)
@@ -113,19 +153,35 @@ namespace GoldenTicket.Controllers
             return View(applicant);
         }
 
+        /**
+         * <summary>
+         * Display a form asking for which schools the applicant wants to enter their child.
+         * </summary>
+         * */
         public ActionResult SchoolSelection()
         {
+            // Validate the session
             if (!IsActiveSession()) //TODO Do this with AOP/Annotations instead
             {
                 return RedirectToAction("Index");
             }
 
+            // Setup view variables
             var applicant = GetSessionApplicant();
             viewHelper.PrepareSchoolSelectionView(ViewBag, applicant);
 
+            // Display the form
             return View(applicant); 
         }
 
+        /**
+         * <summary>
+         * Save school selections for applicant
+         * </summary>
+         * 
+         * <param name="applicant">Applicant object with applicant ID</param>
+         * <param name="formCollection">Contains checkbox selections for which schools to register the applicant for</param>
+         * */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult SchoolSelection(Applicant applicant, FormCollection formCollection)
@@ -175,20 +231,33 @@ namespace GoldenTicket.Controllers
             return RedirectToAction("Review");
         }
 
+        /**
+         * <summary>
+         * Shows the application review page.
+         * </summary>
+         * */
         public ActionResult Review()
         {
+            // Validate the session
             if(!IsActiveSession()) //TODO Do this with AOP/Annotations instead
             {
                 return RedirectToAction("Index");
             }
 
+            // Populate variables for view
             var applicant = GetSessionApplicant();
-            
             PrepareReviewView(applicant);
 
+            // Display stuff
             return View(applicant);
         }
 
+        /**
+         * <summary>
+         * Generate a confirmation code and save the language the application was filled out in
+         * (helps later when sending a potentially translated email).
+         * </summary>
+         * */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Review(Applicant applicant)
@@ -199,6 +268,7 @@ namespace GoldenTicket.Controllers
                 return RedirectToAction("Index");
             }
 
+            // Generate a confirmation code and save the language
             applicant.ConfirmationCode = Guid.NewGuid().ToString().Split('-')[0].ToUpper();
             applicant.Language = CultureInfo.CurrentUICulture.Name;
             SaveReview(applicant);
@@ -206,17 +276,27 @@ namespace GoldenTicket.Controllers
             return RedirectToAction("Confirmation");
         }
 
+        /**
+         * <summary>
+         * Display a confirmation page for the applicant, along with their confirmation code.
+         * Also, send a confirmation email.
+         * </summary>
+         * */
         public ActionResult Confirmation()
         {
+            // Validate the session
             if (!IsActiveSession()) //TODO Do this with AOP/Annotations instead
             {
                 return RedirectToAction("Index");
             }
 
+            // Get the applicant
             var applicant = GetSessionApplicant();
 
+            // Clear the session
             Session.Clear();
 
+            // Send an email with confirmation code and messages
             var globalConfig = GetGlobalConfig();
             ViewBag.GlobalConfig = globalConfig;
 
@@ -233,12 +313,20 @@ namespace GoldenTicket.Controllers
                 EmailHelper.SendEmail(applicant.Contact2Email, "RI Pre-K Lottery Confirmation", messageBody);    
             }
 
+            // Display confirmation page
             return View(applicant);
         }
 
         // ---- Helper Fields ----
 
-
+        /**
+         * <summary>
+         * Saves student informatin fields to an existing applicant in the database.
+         * Different from the out-of-the-box AddOrUpdate in that fields not populated during
+         * this step will not get overwritten with null in the database. In other words, it
+         * only saves the relevant fields with student information without touching the other fields.
+         * </summary>
+         * */
         private void SaveStudentInformation(Applicant applicant)
         {
             // Add a new applicant
@@ -263,10 +351,19 @@ namespace GoldenTicket.Controllers
                 applicantEntry.Property(a => a.StudentGender).IsModified = true;
             }
 
+            // Save and put the applicant ID into the session
             database.SaveChanges();
             Session["applicantID"] = applicant.ID;
         }
 
+        /**
+         * <summary>
+         * Gets the applicant associated with the session. If there is no applicant associated
+         * with the session, creates a new one so that an ID can be associated with the session.
+         * </summary>
+         * 
+         * <returns>The applicant associated with the current session</returns>
+         */
         private Applicant GetSessionApplicant()
         {
             Applicant applicant = null;
@@ -283,11 +380,23 @@ namespace GoldenTicket.Controllers
             return applicant;
         }
 
+        /**
+         * <summary>
+         * Verifies that data coming in from a form is associated with the applicant that the session was
+         * started with. This prevents a malicious user from manually overriding the hidden ID field on
+         * most pages with that of a different applicant's ID. This not only prevents them from getting back
+         * information about applicants already in the database, but from modifying applications that aren't their
+         * own.
+         * </summmary>
+         * 
+         * <param name="applicant">Applicant with ID field that is incoming from a submission</param>
+         * <returns>True if valid/authorized, false otherwise</returns>
+         */
         private bool IsAuthorizedApplicant(Applicant applicant)
         {
             // Make sure that the student is the one the user is authorized to make (i.e. if an ID is given, it should be the same one in the session)
-            bool isApplicantNew = applicant.ID == 0;
-            bool isActiveSession = Session["applicantID"] != null;
+            var isApplicantNew = applicant.ID == 0;
+            var isActiveSession = Session["applicantID"] != null;
 
             // If a new sessions
             if (isApplicantNew && !isActiveSession)
@@ -296,10 +405,17 @@ namespace GoldenTicket.Controllers
             }
 
             // If existing session, check to make sure session applicant ID matches the one submitted
-            bool isActiveApplicantSameAsSubmitted = applicant.ID.Equals(Session["applicantID"]);
+            var isActiveApplicantSameAsSubmitted = applicant.ID.Equals(Session["applicantID"]);
             return !isApplicantNew && isActiveSession && isActiveApplicantSameAsSubmitted;
         }
-
+        /**
+          * <summary>
+          * Saves guardian information fields to an existing applicant in the database.
+          * Different from the out-of-the-box AddOrUpdate in that fields not populated during
+          * this step will not get overwritten with null in the database. In other words, it
+          * only saves the relevant fields with student information without touching the other fields.
+          * </summary>
+          * */
         private void SaveGuardianInformation(Applicant applicant)
         {
             database.Applicants.Attach(applicant);
@@ -321,6 +437,11 @@ namespace GoldenTicket.Controllers
             database.SaveChanges();
         }
 
+        /**
+         * <summary>
+         * Prepares the ViewBag for display the application review page. (List of schools and notification date)
+         * </summary>
+         */
         private void PrepareReviewView(Applicant applicant)
         {
             var applieds = database.Applieds.Where(a => a.ApplicantID == applicant.ID).ToList();
@@ -330,9 +451,17 @@ namespace GoldenTicket.Controllers
 
             ViewBag.Programs = programs;
 
-            ViewBag.NotificationDate = config.NotificationDate;
+            ViewBag.NotificationDate = database.GlobalConfigs.First();
         }
 
+        /**
+        * <summary>
+        * Saves confirmation and language fields to an existing applicant in the database.
+        * Different from the out-of-the-box AddOrUpdate in that fields not populated during
+        * this step will not get overwritten with null in the database. In other words, it
+        * only saves the relevant fields with confirmation and languages fields without touching the other fields.
+        * </summary>
+        * */
         private void SaveReview(Applicant applicant)
         {
             database.Applicants.Attach(applicant);
@@ -344,11 +473,24 @@ namespace GoldenTicket.Controllers
             database.SaveChanges();
         }
 
+        /**
+         * <summary>
+         * Is the view associated with an active session?
+         * </summary>
+         * <returns>True if a session is active, false otherwise</returns>
+         */
         private bool IsActiveSession()
         {
             return Session["applicantID"] != null;
         }
 
+        /**
+         * <summary>
+         * Gets the global configuration.
+         * </summary>
+         * 
+         * <returns>The global configuration for the application</returns>
+         */
         private GlobalConfig GetGlobalConfig()
         {
             return database.GlobalConfigs.First();
