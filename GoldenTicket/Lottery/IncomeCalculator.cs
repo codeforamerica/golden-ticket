d2b515ce90d7c716a66f08f329c55f1d5eaf1d74
Jeff Maher <jeff.m.maher@gmail.com>
2014-12-07 16:28:40 -0800

    Documented Lottery package

--- GoldenTicket/Lottery/IncomeCalculator.cs
+++ GoldenTicket/Lottery/IncomeCalculator.cs
@@ -19,13 +19,24 @@ namespace GoldenTicket.Lottery
      **/
     public class IncomeCalculator
     {
+        // Multiplier to be applied for every household member above 10 people
         private double above10Multipler;
 
+        // Database connection
         private GoldenTicketDbContext db;
 
+        // Poverty line information
+        // key => num househould members, val => poverty line income amount
         private Dictionary<int, int> povertyLineByNumPeople = new Dictionary<int, int>();
-            // key => num househould members, val => poverty line income amount
-
+            
+        /**
+         * <summary>
+         * Creates a new income calculator based on the poverty line
+         * configuration in the database
+         * </summary>
+         * 
+         * <param name="db">Database connection</param>
+         */
         public IncomeCalculator(GoldenTicketDbContext db)
         {
             this.db = db;
@@ -35,10 +46,15 @@ namespace GoldenTicket.Lottery
                 povertyLineByNumPeople.Add(i, db.PovertyConfigs.Find(i).MinimumIncome);
             }
 
-            GlobalConfig globalConfig = db.GlobalConfigs.First();
+            var globalConfig = db.GlobalConfigs.First();
             above10Multipler = globalConfig.IncomeMultiplier;
         }
 
+        /**
+         * <param name="numHouseholdMembers">Number of people in the household</param>
+         * <param name="incomeAmount">Combined average income of all household members per month</param>
+         * <returns>True if income amount and number of household members equals being below the poverty line</returns>
+         */
         public bool IsBelowPovertyLine(int numHouseholdMembers, int incomeAmount)
         {
             if (numHouseholdMembers > 10)
@@ -52,6 +68,10 @@ namespace GoldenTicket.Lottery
             return incomeAmount <= povertyLineByNumPeople[numHouseholdMembers];
         }
 
+        /**
+         * <param name="applicant">Applicant with household fields populated</param>
+         * <returns>True if income amount and number of household members equals being below the poverty line</returns>
+         */
         public bool IsBelowPovertyLine(Applicant applicant)
         {
             return IsBelowPovertyLine(applicant.HouseholdMembers, applicant.HouseholdMonthlyIncome);
--- GoldenTicket/Lottery/ListShuffle.cs
+++ GoldenTicket/Lottery/ListShuffle.cs
@@ -4,17 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Fisher-Yates algorithm implementation from http://stackoverflow.com/a/22668974/249016
 namespace GoldenTicket.Lottery
 {
+    /**
+     * <summary>
+     * Extension methods for IList<T> objects to allow for random shuffling of their elements.
+     * Fisher-Yates algorithm implementation from http://stackoverflow.com/a/22668974/249016
+     * </summary>
+     */
     public static class ListShuffle
     {
+        /**
+         * <summary>Randomly shuffles the order of elements in a list</summary>
+         * <param name="list">List to shuffle</param>
+         * <param name="rnd">Random number generator</param>
+         */
         public static void Shuffle<T>(this IList<T> list, Random rnd)
         {
             for (var i = 0; i < list.Count; i++)
                 list.Swap(i, rnd.Next(i, list.Count));
         }
 
+        /**
+         * <summary>Swap two elements order in the list</summary>
+         * <param name="list">List to swap elements in</param>
+         * <param name="i">First element to swap</param>
+         * <param name="j">Second element to swap</param>
+         */
         public static void Swap<T>(this IList<T> list, int i, int j)
         {
             var temp = list[i];
